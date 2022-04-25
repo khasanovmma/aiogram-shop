638dc5197dbc2ee4c8b8f9244039eb8075661067
khasanovmma <khasanovmma010@gamil.com>
2022-04-25 17:57:04 +0500

    heroku deploy end

--- handlers/users/account.py
+++ handlers/users/account.py
@@ -5,7 +5,7 @@ from keyboards.inline.inline_menu import generate_menu
 from loader import dp, db
 
 
-@dp.callback_query_handler(IsPrivate(), text='my_account')
+@dp.callback_query_handler(text='my_account')
 async def my_account(call: CallbackQuery):
     chat_id = call.from_user.id
     full_name = await db.user_info(chat_id)
