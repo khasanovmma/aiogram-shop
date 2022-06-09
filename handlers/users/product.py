4acfcd2a3247bab4faac722d822f4e41ef7fa316
Abdulboriy <69503181+khasanovmma@users.noreply.github.com>
2022-06-09 09:00:55 +0500

    Update product.py

--- handlers/users/product.py
+++ handlers/users/product.py
@@ -9,7 +9,7 @@ async def view_product(call: CallbackQuery):
     await call.answer()
     product_id = call.data
     data = await db.get_product_by_id(product_id)
-    product_name, product_photo, product_desc, product_price = data[1], data[2], data[3], data[5]
+    product_name, product_photo, product_desc, product_price = data[1], data[2], data[3], data[4]
     btn = await product(product_id)
 
     await call.message.edit_text(f'<b>{product_name}</b>\n\n'
